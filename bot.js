92efb9da6f4dd40a92aab7b05c8bd35f63f7a24c
agungTuanany <agung.tuanany@gmail.com>
2020-05-05 02:23:18 +0700

    Bot Comman Method

--- bot.js
+++ bot.js
@@ -25,22 +25,9 @@ bot.settings ( (ctx) => {
 	ctx.reply ("you have entered settings commands")
 })
 
-bot.use ( async (ctx, next) => {
-	const start = new Date ()
-	await next ()
-	const ms = new Date () - start
-
-	// Using This information can make response more dynamic
-	console.log ("Response time: %sms", ms)
-	console.log ("ctx-from", ctx.from)
-	console.log ("ctx-chat", ctx.chat)
-	console.log ("ctx-mesagge", ctx.message)
-	console.log ("ctx-updateSubTypes", ctx.updateSubTypes)
-})
-
-bot.on ("text", (ctx) => {
-	ctx.reply (`Hello ${ctx.from.first_name},you have send me a message
-	your message is a "${ctx.updateSubTypes[0]} "
+// Create new command
+bot.command (["test", "Test", "test1"], (ctx) => {
+	ctx.reply (` Hello from new Command
 	`)
 })
 
