68af2dd26950be65b150c2812203cee7d0f7c210
agungTuanany <agung.tuanany@gmail.com>
2020-05-06 02:16:28 +0700

    Chapter : Echoboot | Create helper Function logging

--- echoo_bot/bot.js
+++ echoo_bot/bot.js
@@ -20,16 +20,19 @@ Say something ..
 `
 
 bot.start ( (ctx) => {
+	logger (ctx)
 	ctx.reply ("Hi I am echo Bot")
 	ctx.reply (helpMessage)
 })
 
 bot.help ( (ctx) => {
+	logger (ctx)
 	ctx.reply (helpMessage)
 
 })
 
 bot.command ("echo", (ctx) => {
+	logger (ctx)
 	let input = ctx.message.text
 	let inputArray = input.split (" ")
 	console.log ("inputArray", inputArray)
@@ -45,6 +48,14 @@ bot.command ("echo", (ctx) => {
 
 	ctx.reply (message)
 })
+
+
+// helpers functions
+function logger (ctx) {
+	// console.log ("Someone used daunEchoBot")
+	// console.log (ctx.chat)
+	console.log (`${ctx.from.username} said: ${ctx.message.text}`)
+}
 
 // init bot
 bot.launch ()
