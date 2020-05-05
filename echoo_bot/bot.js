b9942f1f44348d80b4eff57de7940230e6dea631
agungTuanany <agung.tuanany@gmail.com>
2020-05-06 02:33:33 +0700

    Chapter_2 : Echoboot : Using middleware

--- echoo_bot/bot.js
+++ echoo_bot/bot.js
@@ -12,6 +12,17 @@ const env			= require ("./../lib/.env")
 // Instantiate bot
 const bot			= new Telegraf (env.TELEGRAF_API_ECHO_BOT)
 
+// Using bot middleware
+bot.use ( (ctx, next) => {
+	if (ctx.updateSubTypes [0] == "text") {
+		console.log (`${ctx.from.username} said: ${ctx.message.text}`)
+	}
+	else {
+		console.log (`${ctx.from.username} sent a ${ctx.updateSubTypes [0] }`)
+	}
+	next ()
+})
+
 
 const helpMessage = `
 Say something ..
@@ -20,19 +31,16 @@ Say something ..
 `
 
 bot.start ( (ctx) => {
-	logger (ctx)
 	ctx.reply ("Hi I am echo Bot")
 	ctx.reply (helpMessage)
 })
 
 bot.help ( (ctx) => {
-	logger (ctx)
 	ctx.reply (helpMessage)
 
 })
 
 bot.command ("echo", (ctx) => {
-	logger (ctx)
 	let input = ctx.message.text
 	let inputArray = input.split (" ")
 	console.log ("inputArray", inputArray)
@@ -48,14 +56,6 @@ bot.command ("echo", (ctx) => {
 
 	ctx.reply (message)
 })
-
-
-// helpers functions
-function logger (ctx) {
-	// console.log ("Someone used daunEchoBot")
-	// console.log (ctx.chat)
-	console.log (`${ctx.from.username} said: ${ctx.message.text}`)
-}
 
 // init bot
 bot.launch ()
