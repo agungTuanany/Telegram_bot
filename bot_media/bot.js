845e7f8dc431f7882bca3171ec403eeaf7498407
agungTuanany <agung.tuanany@gmail.com>
2020-05-06 23:52:41 +0700

    Chapter_3: Media Bot (working with file) | send gif with sendAnimation method

--- bot_media/bot.js
+++ bot_media/bot.js
@@ -47,6 +47,16 @@ bot.command ("newyork", (ctx) => {
 	)
 })
 
+bot.command ("dubai", (ctx) => {
+	bot.telegram.sendChatAction (ctx.chat.id, "upload_video")
+	bot.telegram.sendAnimation (ctx.chat.id,
+		"https://media2.giphy.com/media/c0BdI069vyn5K/giphy.gif?cid=790b7611640372d3186cd2314995cb37839375a907f0a08e&rid=giphy.gif"
+		,{
+			reply_to_message_id: ctx.message.message_id
+		}
+	)
+})
+
 
 // Init bot
 bot.launch ()
