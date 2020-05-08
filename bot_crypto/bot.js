0e66ab8ea7e08a0f0775ce15b742e57c5b405ed7
agungTuanany <agung.tuanany@gmail.com>
2020-05-09 00:16:03 +0700

    Chapter_6: Crypto bot (Inline & Reply Keyboard) | Interactive Menu

--- bot_crypto/bot.js
+++ bot_crypto/bot.js
@@ -16,23 +16,68 @@ const env			= require ("./../lib/.env")
 const bot = new Telegraf (env.TELEGRAF_API_CRYPTO_BOT)
 
 bot.command ("test", (ctx) => {
-	bot.telegram.sendMessage (ctx.chat.id, "Welcome",
+	bot.telegram.sendMessage (ctx.chat.id, "Main Menu",
 		{
 			reply_markup: {
 				inline_keyboard: [
 					[
-						{ text: "Click me", callback_data: "one" }
+						{ text: "See Fruits List", callback_data: "fruits" }
+					],
+					[
+						{ text: "See Meats List", callback_data: "meats" }
 					]
 				]
 			}
 		})
 })
 
-bot.action ("one", (ctx) => {
-	ctx.answerCbQuery ("Popup message")
-	ctx.reply ("You clicked the button")
+bot.action ("fruits", (ctx) => {
+	ctx.deleteMessage ()
+	ctx.answerCbQuery ("fruits")
+	bot.telegram.sendMessage (ctx.chat.id, "List of fruits: \n -Banana\n -Oranges\n -Grapes",
+		{
+			reply_markup: {
+				inline_keyboard: [
+					[
+						{ text: "Back to menu", callback_data: "menu" }
+					],
+				]
+			}
+		})
 })
 
+bot.action ("meats", (ctx) => {
+	ctx.deleteMessage ()
+	ctx.answerCbQuery ("meats")
+	bot.telegram.sendMessage (ctx.chat.id, "List of meats: \n -Tenderloin\n -Sirloin\n -Steak",
+		{
+			reply_markup: {
+				inline_keyboard: [
+					[
+						{ text: "Back to menu", callback_data: "menu" }
+					],
+				]
+			}
+		})
+})
+
+bot.action ("menu", (ctx) => {
+	ctx.deleteMessage ()
+	ctx.answerCbQuery ("Main Menu")
+	bot.telegram.sendMessage (ctx.chat.id, "Main Menu",
+		{
+			reply_markup: {
+				inline_keyboard: [
+					[
+						{ text: "See Fruits List", callback_data: "fruits" }
+					],
+					[
+						{ text: "See Meats List", callback_data: "meats" }
+					]
+				]
+			}
+		})
+})
 
 
 
