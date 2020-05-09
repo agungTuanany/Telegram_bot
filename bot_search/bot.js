71ab7097eba8b90850b6b4522e0fa85fbe02470e
agungTuanany <agung.tuanany@gmail.com>
2020-05-10 03:56:58 +0700

    Chapter_8: Search bot (Inline Query) | Create Start and Help Command

--- bot_search/bot.js
+++ bot_search/bot.js
@@ -18,6 +18,37 @@ bot.command ("test", (ctx) => {
 	ctx.reply ("Hello wolrd")
 })
 
+//handler for /start and /help command
+bot.command(['start', 'help'], ctx => {
+	//set welcome message
+	const message = `
+Welcome to Search Bot!
+Use the inline mode below
+@s300bot pixa <search image>
+@s300bot wiki <search wiki>
+`;
+
+	//ctx.reply(text, [extra params])
+	ctx.reply(message, {
+		reply_markup: {
+			inline_keyboard: [
+				[
+					// Use switch inline query current chat to pre-type "@s300bot p "
+					{ text: 'Search Pixabay Image', switch_inline_query_current_chat: 'pixa ' }
+				],
+				[
+					// Use switch inline query current chat to pre-type "@s300bot w "
+					{ text: 'Search Wiki', switch_inline_query_current_chat: 'wiki ' }
+				]
+			]
+		}
+	})
+})
+
+
+
+
+
 bot.inlineQuery (/pixa\s.+/, async (ctx) => {
 	// Split string by spaces into an array
 	let input = ctx.inlineQuery.query.split (" ")
