5a5363021d10f1f7d49612fcf5a75a9b182447c6
agungTuanany <agung.tuanany@gmail.com>
2020-05-10 03:51:33 +0700

    Chapter_8: Search bot (Inline Query) | Enable Pixabay & Wiki by using InlineQuery method

--- bot_search/bot.js
+++ bot_search/bot.js
@@ -18,31 +18,36 @@ bot.command ("test", (ctx) => {
 	ctx.reply ("Hello wolrd")
 })
 
-//bot.on ("inline_query", async (ctx) => {
-//	const query = ctx.inlineQuery.query
-//
-//	const res = await axios.get (`https://pixabay.com/api/?key=${apikey}&q=${query}`)
-//	const data = res.data.hits
-//	console.log (data)
-//
-//	let result = data.map ( (item, index) => {
-//		return {
-//			type				: 'photo',
-//			id					: String(index),
-//			photo_url			: item.webformatURL,
-//			thumb_url			: item.previewURL,
-//			photo_width			: 300,
-//			photo_height		: 200,
-//			caption				: `[Source](${item.webformatURL})\n[Large Image](${item.largeImageURL})`,
-//			parse_mode			: 'Markdown'
-//		}
-//	})
-//
-//	ctx.answerInlineQuery (result)
-//})
+bot.inlineQuery (/pixa\s.+/, async (ctx) => {
+	// Split string by spaces into an array
+	let input = ctx.inlineQuery.query.split (" ")
+	// Remove first element in an array
+	input.shift ()
+	const query = input.join (" ")
 
-bot.on ("inline_query", async (ctx) => {
-	const query = ctx.inlineQuery.query
+	const res = await axios.get (`https://pixabay.com/api/?key=${apikey}&q=${query}`)
+	const data = res.data.hits
+
+	let result = data.map ( (item, index) => {
+		return {
+			type				: 'photo',
+			id					: String(index),
+			photo_url			: item.webformatURL,
+			thumb_url			: item.previewURL,
+			photo_width			: 300,
+			photo_height		: 200,
+			caption				: `[Source](${item.webformatURL})\n[Large Image](${item.largeImageURL})`,
+			parse_mode			: 'Markdown'
+		}
+	})
+
+	ctx.answerInlineQuery (result)
+})
+
+bot.inlineQuery (/wiki\s.+/, async (ctx) => {
+	let input = ctx.inlineQuery.query.split (" ")
+	input.shift ()
+	const query = input.join (" ")
 	const res = await axios.get(`https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${query}&limit=50`);
 
 	const data = res.data
